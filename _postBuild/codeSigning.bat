b6d3c5f4d364921d5855477b9bd10388b93f5c37
ksobon <sobon.konrad@gmail.com>
2019-05-05 20:22:48 -0400

    add ability to invoke the code signing script from subdirectories with spaces in file path

--- _postBuild/codeSigning.bat
+++ _postBuild/codeSigning.bat
@@ -8,8 +8,8 @@ set PFX_PATH="C:\Users\ksobon\source\repos\HOK-Revit-Addins\_cert\archilabCertif
 set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\HOK"
 set VALUE_NAME="certificatePassword"
 
-for /f "tokens=3" %%a in ('reg query %KEY_NAME% /V %VALUE_NAME% ^|findstr /ri "REG_SZ"') do set PFX_PASS=%%a
+for /f "tokens=3" %%a in ('%windir%\Sysnative\reg query %KEY_NAME% /V %VALUE_NAME% ^|findstr /ri "REG_SZ"') do set PFX_PASS=%%a
 
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x86\signtool.exe" sign /f "%PFX_PATH%" /p %PFX_PASS% /t http://timestamp.comodoca.com/authenticode "%DLL_PATH%"
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x86\signtool.exe" sign /f "%PFX_PATH%" /p %PFX_PASS% /t http://timestamp.comodoca.com/authenticode %DLL_PATH%
 
 endlocal
